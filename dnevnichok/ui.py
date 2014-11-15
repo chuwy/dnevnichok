23960b2477e8e572a603ae3ced65a75913874d0e
Anton Parkhomenko <anton@chuwy.ru>
2014-11-15 16:04:52 +0700

    Factor out almost everything ncurses-related from App class. Prepare for prettyification.

--- dnevnichok/ui.py
+++ dnevnichok/ui.py
@@ -8,8 +8,9 @@ from dnevnichok.aux import PagedItems, EventQueue
 class ItemList:
     """
     Pane with list of dirs, tags, files or god knows what else
+    TODO: Initialize empty
     """
-    def __init__(self, scr, items):
+    def __init__(self, scr, items=None):
         self.scr = scr
         self.Y, self.X = self.scr.getmaxyx()
         self.cur_item = 0
@@ -27,6 +28,7 @@ class ItemList:
             else: self.render_item(i, item)
 
     def render_item(self, position, item, reverse=False):
+        import os
         def polute(s):
             l = self.width - len(s)
             if l > 0:
@@ -35,7 +37,11 @@ class ItemList:
         if reverse:
             self.scr.addstr(position, 0, polute(item['title'][:self.width]), curses.A_REVERSE)
         else:
-            self.scr.addstr(position, 0, polute(item['title'][:self.width]))
+            if os.path.isdir(item['full_path']):
+                a = curses.color_pair(3)
+            else:
+                a = curses.color_pair(6)
+            self.scr.addstr(position, 0, polute(item['title'][:self.width]), a)
         self.scr.refresh()
 
     def switch_items(self, items, cur_item=0):
@@ -55,6 +61,7 @@ class ItemList:
             self.cur_item += i
         elif i > 0 and self.cur_item == self.length-1 and not self._items.has_next():
             curses.beep()
+            return
         elif i > 0 and self.cur_item == self.length-1 and self._items.has_next():
             self.cur_item = 0
             self._items.next()
@@ -64,16 +71,21 @@ class ItemList:
             self.cur_item += i  # i is negative
         elif i < 0 and self.cur_item == 0 and not self._items.has_prev():
             curses.beep()
+            return
         elif i < 0 and self.cur_item == 0 and self._items.has_prev():
             self.cur_item = self.length-1
             self._items.prev()
             self.render()
+        # self.onMove()
 
     def move_to(self, i):
         if i < 0:
             i = self.length - i - 2
         self.move_highlight(i)
         self.cur_item = i
+
+    def onMove(self, func):
+        func(items[self.cur_item])
 
     def process_keypress(self, c):
         if type(c) is int:                  # Arrow-keys
@@ -96,3 +108,74 @@ class ItemList:
             else: return False
         else: return False
         return True
+
+
+class InfoBar:
+    def __init__(self, scr):
+        self.scr = scr
+        height, self.width = self.scr.getmaxyx()
+        self.Y = height - 2
+
+    def render_item_info(self, item):
+        def fill(el, width):
+            length = len(el)
+            if length >= width:
+                return el
+            else:
+                remain = width - len(el)
+                return el + ' ' * remain
+
+        self.scr.addstr(fill(item.name, 20))
+
+    def print(self, text):
+        if text:
+            self.scr.addstr(self.Y, 1, text)
+
+    def input(self, prompt=''):
+        curses.echo()
+        self.print(prompt)
+        input = self.scr.getstr(self.Y, len(prompt), 20).decode('unicode_escape')
+        curses.noecho()
+        self.clear()
+        return input
+
+    def clear(self):
+        self.scr.move(self.Y, 0)
+        self.scr.clrtoeol()
+
+
+class MainWindow:
+    def __init__(self, scr):
+        self.stdscr = scr
+        self.stdscr.clear()
+        curses.curs_set(0)
+        if curses.has_colors():
+            curses.init_pair(1, curses.COLOR_BLUE, 0)
+            curses.init_pair(2, curses.COLOR_CYAN, 0)
+            curses.init_pair(3, curses.COLOR_GREEN, 0)
+            curses.init_pair(4, curses.COLOR_MAGENTA, 0)
+            curses.init_pair(5, curses.COLOR_RED, 0)
+            curses.init_pair(6, curses.COLOR_YELLOW, 0)
+            curses.init_pair(7, curses.COLOR_WHITE, 0)
+        self.bar = InfoBar(scr)
+        stdscr_y, stdscr_x = self.stdscr.getmaxyx()
+        subwin = self.stdscr.subwin(stdscr_y - 3, stdscr_x, 0, 0)
+
+        self.left_pane = ItemList(subwin)
+        # self.left_pane.onMove(self.menu.render_item_info)
+
+    def print(self, text):
+        self.bar.print(text)
+
+    def input(self, prompt=None):
+        return self.bar.input(prompt)
+
+    def clear_bar(self):
+        self.bar.clear()
+
+    def show_items(self, items, cur_item=0):
+        self.left_pane.switch_items(items, cur_item)
+        self.left_pane.render()
+
+    def process_keypress(self, c):
+        return self.left_pane.process_keypress(c)
